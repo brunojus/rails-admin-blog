32aba883eb296a9888991e66a860e26b1bf9744e
Bruno <bruno_justino@icloud.com>
2016-07-01 16:41:38 -0300

    Iniciando o CRUD do admin

--- app/controllers/admin/moderators_controller.rb
+++ app/controllers/admin/moderators_controller.rb
@@ -1,7 +1,13 @@
 class Admin::ModeratorsController  < ApplicationController
     def index
         @moderators = Moderator.all
+    end
 
+    def edit
+        @moderator = Moderator.find(params[:id])
+    end
+
+    def update
     end
 
 end
